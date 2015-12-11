0f57647af7f6aee142f217466f950349c9d37808
matas_valuzis <matas.valuzis@gmail.com>
2015-12-11 19:30:15 +0200

    added gulp task error handling adn new browserify-prod task

--- src/components/MainFrame.jsx
+++ src/components/MainFrame.jsx
@@ -4,7 +4,7 @@ var MainFrame = React.createClass({
 	render: function() {
         return (
 			<div>
-			Hello worldssfdsf
+			Hello worldssfdsfd
 			</div>
 		);
     }
--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,12 +2,35 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var fs = require('fs');
+var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
 
 gulp.task('browserify', function() {
+	return browserify('./app.jsx')		
+		.transform(babelify, {presets: ["react"]})		
+		.bundle()
+		.on("error", function(err){
+			console.log(err.message);
+			this.emit("end");
+		})
+		.pipe(fs.createWriteStream("../scripts/app.js"));
+});
+
+gulp.task('browserify-prod', function() {
 	return browserify('./app.jsx')
+		.on('error', console.log)
 		.transform(babelify, {presets: ["react"]})
 		.bundle()
-		.pipe(fs.createWriteStream("../scripts/app.js"));
+		.on("error", function(err){
+			console.log(err.message);
+			this.emit("end");
+		})
+		.pipe(source('app.js'))
+		.pipe(buffer())
+		.pipe(uglify())		
+		.pipe(gulp.dest("../scripts"));
 });
 
 gulp.task('watch', function(){
